fc00e718b72fc8be1d140381f5f4d127cd037b38
Hilmar Falkenberg <hilmar.falkenberg@sap.com>
2026-04-14 16:39:01 +0200

    adjust interface
    add defaults for size() and isEmpty()
    
    Signed-off-by: Hilmar Falkenberg <hilmar.falkenberg@sap.com>

--- sdk/logs/src/main/java/io/opentelemetry/sdk/logs/export/AuditLogStore.java
+++ sdk/logs/src/main/java/io/opentelemetry/sdk/logs/export/AuditLogStore.java
@@ -16,4 +16,22 @@ public interface AuditLogStore {
   void removeAll(Collection<LogRecordData> logs);
 
   Collection<LogRecordData> getAll();
+
+  /**
+   * Delegates to size of {@link #getAll()}.
+   *
+   * @return Returns the number of log records in the store.
+   */
+  default int size() {
+    return getAll().size();
+  }
+
+  /**
+   * Delegates to {@link #size()} == 0.
+   *
+   * @return Returns true if the store contains no log records.
+   */
+  default boolean isEmpty() {
+    return size() == 0;
+  }
 }
